d15d45c5eb10369c5feb405a7c1f5b1847079dcc
KhemPha <khempha@KhemPhas-MacBook-Pro.local>
2020-02-15 12:34:25 +0700

    first commit

--- src/app/dummy/status.ts
+++ src/app/dummy/status.ts
@@ -1,26 +1,32 @@
 export const StatusObj = {
   DISABLED: { key: 0, text: "Disabled" },
-  ACTIVE: { key: 1, text: "Active" },
+  ACTIVE: { key: 1, text: "Active" }
 };
 
 export const BOOK_OPTIONS = [
   { key: 2, text: "Top Rating", route: "RATING" },
   { key: 3, text: "New Arrival", route: "NEW_ARRIVAL" },
   { key: 4, text: "Best Novel", route: "BEST_NOVEL" },
-  { key: 5, text: "Top Free", route: "FREE" },
-]
+  { key: 5, text: "Top Free", route: "FREE" }
+];
 
 export const BOOK_STATUS = [
   { key: 1, text: "Completed" },
-  { key: 2, text: "Progress" },
-]
+  { key: 2, text: "Progress" }
+];
 
 export const MOBILE_ADVERTISEMENT_TYPE = [
-  { key: 1, text: "Article Scroll" },
-  { key: 2, text: "Under Title Detail Page" },
-  { key: 3, text: "Below Pharagraph Page Detail" },
-  { key: 4, text: "Footer" },
-]
+  { key: 1, text: "Ads Header Fix" },
+  { key: 2, text: "Ads List Contents" },
+  { key: 3, text: "Ads Bottom Fix" },
+  { key: 4, text: "Ads Modal Pop-up" },
+  { key: 5, text: "Ads Drawer Bottom Fix" },
+  { key: 6, text: "Ads Detail Top Fix" },
+  { key: 7, text: "Ads Detail Top" },
+  { key: 8, text: "Ads Detail Bottom" },
+  { key: 9, text: "Ads Detail Bottom Fix" },
+  { key: 10, text: "Ads Detail Modal Pop-up" }
+];
 
 export const WEB_ADVERTISEMENT_TYPE = [
   { key: 1, text: "Header" },
@@ -30,105 +36,111 @@ export const WEB_ADVERTISEMENT_TYPE = [
   { key: 5, text: "Popup Detail" },
   { key: 6, text: "Under Title Detail Page" },
   { key: 7, text: "Below Image Detail Page" },
-  { key: 8, text: "Center Paragraph" },
-]
+  { key: 8, text: "Center Paragraph" }
+];
 
 export const PAYMENT_TYPES = [
   // { key: 1, text: "Buy" },
   { key: 1, text: "Subscript" },
-  { key: 2, text: "Free" },
-]
+  { key: 2, text: "Free" }
+];
 export const Advertise_Status = [
   { key: 1, text: "Active" },
   { key: 2, text: "Disactive" },
   { key: 3, text: "Pending" },
-  { key: 4, text: "Expired" },
-]
+  { key: 4, text: "Expired" }
+];
 export const Content_Status = [
   // { key: 1, text: "Buy" },
   { key: 1, text: "Publish" },
   { key: 2, text: "Disable" },
-  { key: 3, text: "Pending" },
-]
+  { key: 3, text: "Pending" }
+];
 
 export const FILTER_OPTIONS = [
   { key: "phone", text: "Phone Number" },
   { key: "fullName", text: "Full Name" },
   { key: "firstName", text: "First Name" },
-  { key: "lastName", text: "Last Name" },
+  { key: "lastName", text: "Last Name" }
 ];
 
 export const PAY_STATUS = {
-  paid: { key: 1, text: 'Paid' },
-  unpaid: { key: 2, text: 'Unpaid' },
-}
+  paid: { key: 1, text: "Paid" },
+  unpaid: { key: 2, text: "Unpaid" }
+};
 
 export const PERMISSION = [
-    {
-      name: "ABOUT",
-      view: false,
-      edit: false,
-
-    }, {
-      name: "CONTACT",
-      view: false,
-      edit: false,
-    }, {
-      name: "CONTENT",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    }, {
-      name: "CATEGORY",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    }, {
-      name: "SUB CATEGORY",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    }, {
-      name: "MOBILE ADVERTISEMENT",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    },{
-      name: "WEB ADVERTISEMENT",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    },{
-      name: "PACKAGES",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    },
-    {
-      name: "ROLE",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    },
-    {
-      name: "USERS",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    },
-    {
-      name: "GALLERY",
-      view: false,
-      create: false,
-      edit: false,
-      delete: false
-    }
-  ];
+  {
+    name: "ABOUT",
+    view: false,
+    edit: false
+  },
+  {
+    name: "CONTACT",
+    view: false,
+    edit: false
+  },
+  {
+    name: "CONTENT",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "CATEGORY",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "SUB CATEGORY",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "MOBILE ADVERTISEMENT",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "WEB ADVERTISEMENT",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "PACKAGES",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "ROLE",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "USERS",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  },
+  {
+    name: "GALLERY",
+    view: false,
+    create: false,
+    edit: false,
+    delete: false
+  }
+];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,21 @@
 export const environment = {
   production: true,
   firebase: {
-      apiKey: "AIzaSyCmtqW5d7GIg2As2TXKxAlz5RJQZSjYdiw",
-      authDomain: "health-cambodia.firebaseapp.com",
-      databaseURL: "https://health-cambodia.firebaseio.com",
-      projectId: "health-cambodia",
-      storageBucket: "health-cambodia.appspot.com",
-      messagingSenderId: "364469294740",
-      appId: "1:364469294740:web:5c82d76d83102938404681"
+    // apiKey: "AIzaSyBwvHaROrNbvcFVqd7vh00CpHye17fLjbE",
+    // authDomain: "yeaksa-news-dd434.firebaseapp.com",
+    // databaseURL: "https://yeaksa-news-dd434.firebaseio.com",
+    // projectId: "yeaksa-news-dd434",
+    // storageBucket: "yeaksa-news-dd434.appspot.com",
+    // messagingSenderId: "32256255896",
+    // appId: "1:32256255896:web:7fd0efb3113e9738276ded",
+    // measurementId: "G-8KMQV51MHY"
+    apiKey: "AIzaSyDx90pA5Bv98cNZPdA3mtEfZhFQNfQOW9Q",
+    authDomain: "immigration-45eeb.firebaseapp.com",
+    databaseURL: "https://immigration-45eeb.firebaseio.com",
+    projectId: "immigration-45eeb",
+    storageBucket: "immigration-45eeb.appspot.com",
+    messagingSenderId: "469609563421",
+    appId: "1:469609563421:web:ebf08eaedee39fed3f8df4"
     },
-    mapKey: 'AIzaSyCmtqW5d7GIg2As2TXKxAlz5RJQZSjYdiw'
+    mapKey: 'AIzaSyBwvHaROrNbvcFVqd7vh00CpHye17fLjbE'
 };
